c865f9711fe34bfddbe623401c288c18275382e8
lookster49 <Lookster_49@yahoo.com>
2016-04-12 17:20:01 +0800

    knex set as database

--- public/ToDoList.js
+++ public/ToDoList.js
@@ -16,7 +16,7 @@ list.create({text: input});
 
 function get_todolist() {
     var todos = new Array;
-    var todos_str = localStorage.getItem('todo');
+    var todos_str = knex.getItem('todo');
     if (todos_str !== null) {
         todos = JSON.parse(todos_str); 
     }
@@ -28,7 +28,7 @@ function createList() {
  
     var todos = get_todoslist();
     todos.push(task);
-    localStorage.setItem('todo', JSON.stringify(todos));
+    knex.setItem('todo', JSON.stringify(todos));
  
     show();
  
